83fb4755b233a613e20342d2ef9ceaab9223e314
Gianluca Zani <zani.gianluca98@gmail.com>
2022-05-02 15:14:27 +1000

    Implemented BlockChain Miner

--- Assignment2_skeleton/BlockchainMiner.py
+++ Assignment2_skeleton/BlockchainMiner.py
@@ -8,10 +8,40 @@ import json
 import hashlib
 import time
 
-class BlockchainMiner():
-    def __init__(self, *args):
+from assignment2.lib.lib import calculate_hash
+
+HOST = "127.0.0.1"
+
+class BlockchainMiner:
+    def __init__(self, current_proof, port, server_port):
+        self.port = port
+        self.server_port = server_port
+        self.current_proof = current_proof
 
     def run(self):
-    
-    def proof_of_work(self):
+        while True:
+            time.sleep(1)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                try:
+                    s.connect((HOST, int(self.server_port)))
+                    message = "gp"  # message is "get proof" request command to send to the server
+                    s.sendall(bytes(message, encoding="utf-8"))
+                    s.listen()  # listen for now messages
+                    blockchain_server, address = s.accept()  # accept connection request
+                    received = blockchain_server.recv(4096)
+                    previous_proof = int(received.decode("utf-8"))
+                    if previous_proof != self.current_proof:
+                        new_proof = self.proof_of_work(previous_proof)
+                        message = f"up|{new_proof}"
+                        s.sendall(bytes(message, encoding="utf-8"))
+                    s.close()
+                except Exception as e:
+                    print(f"Miner {self.port} ERROR COMMUNICATING WITH SERVER {self.server_port}")
+                    continue
 
+    def proof_of_work(self, previous_proof):
+        new_proof = 0
+        while calculate_hash(new_proof ** 2 - previous_proof ** 2)[:2] != "00":
+            new_proof += 1
+        self.current_proof = new_proof
+        return new_proof
