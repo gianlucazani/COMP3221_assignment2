a26cc8806ee8af5c4a7e3736c71de1dcc3e27617
Gianluca Zani <zani.gianluca98@gmail.com>
2022-05-04 12:21:25 +1000

    Worker thread inside Miner

--- Assignment2_skeleton/BlockchainMiner.py
+++ Assignment2_skeleton/BlockchainMiner.py
@@ -1,5 +1,6 @@
 import socket
 import _thread
+import threading
 import time
 
 from assignment2.lib.lib import calculate_hash
@@ -7,11 +8,43 @@ from assignment2.lib.lib import calculate_hash
 HOST = "127.0.0.1"
 
 
+class Worker(threading.Thread):
+    def __init__(self, proof_to_work_on, server_port_no):
+        super().__init__()
+        self.working_on_proof = proof_to_work_on
+        self.server_port_no = server_port_no
+
+
+    def run(self):
+        while self.running:
+            new_proof = 0
+            while self.running and calculate_hash(new_proof ** 2 - self.working_on_proof ** 2)[:2] != "00":
+                new_proof += 1
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((HOST, int(self.server_port_no)))
+                message = f"up|{new_proof}"
+
+                # SEND NEW PROOF TO SERVER
+                try:
+                    s.sendall(bytes(message, encoding="utf-8"))
+                except socket.error as e:
+                    print(f"Miner error SENDING PROOF to server {self.server_port_no}")
+                    print(f"ERROR {e}")
+                    continue
+                s.close()
+
+    def pause(self):
+        self.running = False
+
+    def activate(self):
+        self.running = True
+
 class BlockchainMiner:
     def __init__(self, port, server_port):
         self.port = port
         self.server_port = server_port
-        self.find_new
+        self.prev_proof = 100
+        self.work_on_next_proof = True
 
     def run(self):
         _thread.start_new_thread(self.poll_server())
@@ -67,4 +100,3 @@ class BlockchainMiner:
                     print(f"Miner {self.port} error RECEIVING PROOF to server {self.server_port}")
                     print(f"ERROR {e}")
                     continue
-
